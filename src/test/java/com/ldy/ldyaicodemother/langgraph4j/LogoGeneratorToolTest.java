672ecc8a620abf24ed470cde750d70e27dc240a7
longdeyao <1825257246@qq.com>
2026-07-11 16:16:46 +0800

    #noid fix 处理工作流。

---
+++ src/test/java/com/ldy/ldyaicodemother/langgraph4j/LogoGeneratorToolTest.java
@@ -0,0 +1,35 @@
+package com.ldy.ldyaicodemother.langgraph4j;
+
+import com.ldy.ldyaicodemother.langgraph4j.enums.ImageCategoryEnum;
+import com.ldy.ldyaicodemother.langgraph4j.resource.ImageResource;
+import com.ldy.ldyaicodemother.langgraph4j.tools.LogoGeneratorTool;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+class LogoGeneratorToolTest {
+
+    @Resource
+    private LogoGeneratorTool logoGeneratorTool;
+
+    @Test
+    void testGenerateLogos() {
+        // 测试生成Logo
+        List<ImageResource> logos = logoGeneratorTool.generateLogos("技术公司现代简约风格Logo");
+        assertNotNull(logos);
+        ImageResource firstLogo = logos.getFirst();
+        assertEquals(ImageCategoryEnum.LOGO, firstLogo.getCategory());
+        assertNotNull(firstLogo.getDescription());
+        assertNotNull(firstLogo.getUrl());
+        logos.forEach(logo ->
+                System.out.println("Logo: " + logo.getDescription() + " - " + logo.getUrl())
+        );
+    }
+}
